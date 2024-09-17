4a837b5e17999462d6c2d4a62e416d79c311be74
Rajan Prajapati <Prajapati>
2024-09-17 23:12:44 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -22,71 +22,69 @@ app.use(
   })
 );
 
-
-
 app.use(`${process.env.API}/swiggy`,SwiggyRoute)
 app.use(`${process.env.API}/auth`,AuthRoute)
 
-app.get("/api/restaurants", (req, res) => {
-  const { lat, lng } = req.query;
-  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
+// app.get("/api/restaurants", (req, res) => {
+//   const { lat, lng } = req.query;
+//   const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
 
-  fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("An error occurred");
-    });
-});
+//   fetch(url, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*",
+//       "User-Agent":
+//         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+//     },
+//   })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       res.json(data);
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).send("An error occurred");
+//     });
+// });
 
-app.get("/api/menu", (req, res) => {
-  const { lat, lng, restaurantId } = req.query;
-  console.log(req.query);
+// app.get("/api/menu", (req, res) => {
+//   const { lat, lng, restaurantId } = req.query;
+//   console.log(req.query);
 
-  /* OLD SWIGGY API
-  const url = `https://www.swiggy.com/dapi/menu/v4/full?lat=${lat}&lng=${lng}&menuId=${menuId}`;
-  */
+//   /* OLD SWIGGY API
+//   const url = `https://www.swiggy.com/dapi/menu/v4/full?lat=${lat}&lng=${lng}&menuId=${menuId}`;
+//   */
 
-  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&submitAction=ENTER&restaurantId=${restaurantId}`;
+//   const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&submitAction=ENTER&restaurantId=${restaurantId}`;
 
-  fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("An error occurred");
-    });
-});
+//   fetch(url, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*",
+//       "User-Agent":
+//         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+//     },
+//   })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).send("An error occurred");
+//     });
+// });
 
 app.listen(port, () => {
   MongDbConnect();
